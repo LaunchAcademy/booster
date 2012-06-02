03919eb5e538c33af1c1b64e02d7252236ef96ca
Dan Pickett <dan.pickett@launchware.com>
2012-06-02 20:22:38 -0300

    update date formats too

--- template.rb
+++ template.rb
@@ -303,6 +303,7 @@ initializer 'time_formats.rb',
 %q{# Example time formats
 { :short_date => "%x", :long_date => "%a, %b %d, %Y" }.each do |k, v|
   Time::DATE_FORMATS.update(k => v)
+  Date::DATE_FORMATS.update(k => v)
 end
 }
 
