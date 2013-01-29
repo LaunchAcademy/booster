05c81616e11ac5372074a654c97b5df1204c6dff
Dan Pickett <dan.pickett@launchware.com>
2013-01-28 20:00:12 -0500

    quiet down asset pipeline
    fixes #19

--- template.rb
+++ template.rb
@@ -271,6 +271,24 @@ file 'app/views/layouts/application.html.erb',
 # INITIALIZERS
 #====================
 
+initializer 'asset_pipeline_logging.rb',
+%q{#credit: https://gist.github.com/2409513
+if Rails.env.development?
+  Rails.application.assets.logger = Logger.new('/dev/null')
+
+  Rails::Rack::Logger.class_eval do
+    def call_with_quiet_assets(env)
+      previous_level = Rails.logger.level
+      Rails.logger.level = Logger::ERROR if env['PATH_INFO'] =~ %r{^/assets/}
+      call_without_quiet_assets(env)
+    ensure
+      Rails.logger.level = previous_level
+    end
+    alias_method_chain :call, :quiet_assets
+  end
+
+end
+}
 initializer 'smtp.rb',
 %q{ActionMailer::Base.smtp_settings = {
     :address => "",
