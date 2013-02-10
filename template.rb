018f9ba5751b586e9095f05fbac6dc9f61937bf1
Dan Pickett <dan.pickett@launchware.com>
2013-02-10 15:32:02 -0500

    guardfile

--- template.rb
+++ template.rb
@@ -1070,12 +1070,50 @@ download("https://github.com/downloads/wycats/handlebars.js/handlebars.1.0.0.bet
 # ===========
 # GUARD
 # ===========
-[
-  "bundler",
-  "jasmine"
-].each do |guard_item|
-  run "bundle exec guard init #{guard_item}"
+file 'Guardfile', %q{
+  # A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'bundler' do
+  watch('Gemfile')
+  # Uncomment next line if Gemfile contain `gemspec' command
+  # watch(/^.+\.gemspec/)
 end
+
+### Guard::Konacha
+#  available options:
+#  - :spec_dir, defaults to 'spec/javascripts'
+#  - :driver, defaults to :selenium
+
+require 'capybara/poltergeist'
+guard :konacha,
+  driver: :poltergeist do
+
+  watch(%r{^app/assets/javascripts/(.*)\.js(\.coffee)?$}) { |m| "#{m[1]}_spec.js" }
+  watch(%r{^spec/javascripts/.+_spec(\.js|\.js\.coffee)$})
+end
+
+guard 'rspec',
+  zeus: true,
+  all_on_start: false,
+  all_after_pass: false do
+
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch('spec/spec_helper.rb')  { "spec" }
+
+  # Rails example
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^app/(.*)(\.erb|\.haml)$})                 { |m| "spec/#{m[1]}#{m[2]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/#{m[1]}_routing_spec.rb", "spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb", "spec/acceptance/#{m[1]}_spec.rb"] }
+  watch(%r{^spec/support/(.+)\.rb$})                  { "spec" }
+  watch('config/routes.rb')                           { "spec/routing" }
+  watch('app/controllers/application_controller.rb')  { "spec/controllers" }
+
+  # Capybara features specs
+  watch(%r{^app/views/(.+)/.*\.(erb|haml)$})          { |m| "spec/features/#{m[1]}_spec.rb" }
+end
+}
 
 run 'zeus init'
 
