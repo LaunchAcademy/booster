eb84c7b7af250b0de60c077e6521243e452a8111
Dan Pickett <dan.pickett@launchware.com>
2013-02-16 17:39:08 -0500

    indentation and drop transactional fixtures
    (we're using database cleaner instead)

--- template.rb
+++ template.rb
@@ -421,50 +421,45 @@ file 'app/assets/javascripts/xhr_fix.js',
 
 file 'spec/spec_helper.rb',
 %q{require 'rubygems'
-  require 'postmaster_general'
+require 'postmaster_general'
 
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 
-  require 'pry'
+require 'pry'
 
-  require 'rspec/rails'
-  require 'factory_girl'
-  require 'database_cleaner'
-  require 'shoulda'
-  require 'capybara/rspec'
-  require 'mocha/api'
-  require 'valid_attribute'
+require 'rspec/rails'
+require 'factory_girl'
+require 'database_cleaner'
+require 'shoulda'
+require 'capybara/rspec'
+require 'mocha/api'
+require 'valid_attribute'
 
-  PostmasterGeneral.log_directory = Rails.root.join("tmp/rendered_emails")
+PostmasterGeneral.log_directory = Rails.root.join("tmp/rendered_emails")
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    config.mock_with :mocha
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :mocha
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "spec/fixtures"
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "spec/fixtures"
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
-
-    config.treat_symbols_as_metadata_keys_with_true_values = true
-    config.filter_run focus: true
-    config.run_all_when_everything_filtered = true
-  end
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
 }, :force => true
 
 # ====================
