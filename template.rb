a4119c57a6c5e2792f4d381fbdfac2d0dd435e37
Dan Pickett <dan.pickett@launchware.com>
2014-10-01 14:28:36 -0400

    adopt spec_helper vs. rails_helper separation

--- template.rb
+++ template.rb
@@ -359,18 +359,7 @@ require File.expand_path("../../config/environment", __FILE__)
 
 require 'pry'
 
-require 'rspec/rails'
-require 'factory_girl'
-require 'database_cleaner'
-require 'shoulda'
-require 'capybara/rspec'
 require 'mocha/setup'
-require 'valid_attribute'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -390,6 +379,50 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 end
 }, :force => true
+
+file 'spec/rails_helper.rb', %Q{
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+require 'factory_girl'
+require 'database_cleaner'
+
+require 'shoulda'
+require 'capybara/rspec'
+require 'valid_attribute'
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+end
+}
 
 # ====================
 # CSS
