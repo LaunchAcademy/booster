8fb445dc45670f0ff446ff4f743bb44a891588b7
Dan Pickett <dan.pickett@launchware.com>
2014-10-04 11:18:56 -0400

    remove deprecated directive in rspec configuration

--- template.rb
+++ template.rb
@@ -367,7 +367,6 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :mocha
 
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
